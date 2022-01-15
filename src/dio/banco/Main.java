bc4c7fc9fe9f2f9fd31c643293f7f2c837bf1112
jultty <juffff@tutanota.de>
2022-01-15 15:36:19 -0300

    criação da classe Menu

--- src/dio/banco/Main.java
+++ src/dio/banco/Main.java
@@ -1,75 +1,12 @@
 package dio.banco;
 
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
         // versão 0.1 de 14/01/2022
 
-        Scanner scan = new Scanner(System.in);
-
-        List<String> operacoes = new ArrayList<>();
-
-        operacoes.add("");
-        operacoes.add("criar conta");   operacoes.add("ver saldo");
-        operacoes.add("depositar");     operacoes.add("sacar");
-        operacoes.add("transferir");    operacoes.add("encerrar conta");
-
-        System.out.println("Operações disponíveis:");
-        for (int i = 1; i < 7; i++) {
-            System.out.println("\n (" + i + ") " + operacoes.get(i));
-        }
-
-        // validação
-
-        int entrada = 0;
-        boolean entradaInvalida = true;
-        while (entradaInvalida) {
-            System.out.println("\n\n Digite o número da operação desejada: ");
-            try {
-                String entradaString = scan.nextLine();
-                entrada = Integer.parseInt(entradaString);
-                if (entrada >= 1 && entrada < operacoes.size()) { entradaInvalida = false; }
-            } catch (NumberFormatException e) {
-                System.out.println("[Erro] Valor fornecido não é um número: " + e.getLocalizedMessage());
-            } catch (NullPointerException e) {
-                System.out.println("[Erro] Nenhum valor fornecido: " + e.getLocalizedMessage());
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("[Erro] Digite um valor do menu: " + e.getLocalizedMessage());
-            }
-        }
-            // entrada válida
-
-        String promptSelecao = "Você selecionou: " + operacoes.get(entrada);
-        System.out.println(promptSelecao);
-
-        switch(entrada) {
-            case 1:
-                // ...
-                break;
-            case 2:
-                // ...
-                break;
-            case 3:
-                // ...
-                break;
-            case 4:
-                // ...
-                break;
-            case 5:
-                // ...
-                break;
-            case 6:
-                // ...
-                break;
-            default:
-            System.out.println("Opção inválida");
-                break;
-        }
+        Menu principal = new Menu();
+        principal.menuPrincipal();
 
         /*Cliente ccCliente = new Cliente();
         Cliente cpCliente = new Cliente();
