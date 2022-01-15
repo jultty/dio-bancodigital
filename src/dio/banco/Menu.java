f1f067e5602d7531b92c4b9e4d3349f4ec93bb83
jultty <juffff@tutanota.de>
2022-01-15 16:15:48 -0300

    menu repete opções em caso de erro

--- src/dio/banco/Menu.java
+++ src/dio/banco/Menu.java
@@ -6,27 +6,32 @@ import java.util.Scanner;
 
 public class Menu extends Main {
 
-    protected void menuPrincipal() {
     Scanner scan = new Scanner(System.in);
 
     List<String> operacoes = new ArrayList<>();
-
+    {
         operacoes.add("");
         operacoes.add("criar conta");   operacoes.add("ver saldo");
         operacoes.add("depositar");     operacoes.add("sacar");
         operacoes.add("transferir");    operacoes.add("encerrar conta");
-
-        System.out.println("Operações disponíveis:");
-        for (int i = 1; i < 7; i++) {
-        System.out.println("\n (" + i + ") " + operacoes.get(i));
     }
 
-    // validação
+    protected void menuPrincipalOpcoes() {
+        System.out.println("Operações disponíveis:");
+        for (int i = 1; i < 7; i++) {
+            System.out.println("\n (" + i + ") " + operacoes.get(i));
+        }
+    }
+
+         // validação
+
+    protected void menuPrincipal() {
 
     int entrada = 0;
     boolean entradaInvalida = true;
         while (entradaInvalida) {
-        System.out.println("\n\n Digite o número da operação desejada: ");
+        menuPrincipalOpcoes();
+        System.out.print("\n Digite o número da operação desejada: ");
         try {
             String entradaString = scan.nextLine();
             entrada = Integer.parseInt(entradaString);
@@ -39,7 +44,8 @@ public class Menu extends Main {
             System.out.println("[Erro] Digite um valor do menu: " + e.getLocalizedMessage());
         }
     }
-    // entrada válida
+
+        // entrada válida
 
     String promptSelecao = "Você selecionou: " + operacoes.get(entrada);
         System.out.println(promptSelecao);
